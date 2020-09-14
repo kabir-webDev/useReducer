5e75012f1ec48126729da5c21fa259fd47fccbb8
kabir-webDev <kabir.webdev@gmail.com>
2020-09-14 08:55:20 +0600

    Playing with conditionals

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from "react";
 import "./App.css";
-import { useState } from "react";
-import TextHidden from "./component/Delete/TextHidden";
-import Fetch from "./component/FetchData/Fetch";
-import ComponentA from "./ContextComponent/ComponentA";
-import ComponentB from "./ContextComponent/ComponentB";
-import { UserContext } from "./ContextComponent/UserContext";
-import ComponentE from "./ContextComponent/ComponentE";
-import ReducerPrac from "./ReducerPrac";
-import Another from "./component/Another.JS";
+import JodiEitaTaileOita from "./Conditional/JodiEitaTaileOita";
+// import { useState } from "react";
+// import TextHidden from "./component/Delete/TextHidden";
+// import Fetch from "./component/FetchData/Fetch";
+// import ComponentA from "./ContextComponent/ComponentA";
+// import ComponentB from "./ContextComponent/ComponentB";
+// import { UserContext } from "./ContextComponent/UserContext";
+// import ComponentE from "./ContextComponent/ComponentE";
+// import ReducerPrac from "./ReducerPrac";
+// import Another from "./component/Another.JS";
 
 function App() {
   return (
     <div className="App">
-      {/* <TextHidden text={"This is awesome. This is cool. This is great great greatness. This is fucking awesome. This is cool."} maxlength={11}/> */}
-      {/* <Fetch/> */}
-      {/* Text Loading...
-        <UserContext.Provider value="This is from Component h">
-          <ComponentA/>
-          <ComponentB/> */}
-      {/* <ComponentE/> */}
-      {/* </UserContext.Provider> */}
-      <h1>This is Context API</h1>
-      <Another />
-      <ReducerPrac />
+      {" "}
+      <JodiEitaTaileOita />{" "}
     </div>
   );
 }
---
+++ src/Conditional/JodiEitaTaileOita.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./Walah.css";
+
+function JodiEitaTaileOita() {
+  const [name, setName] = useState("First Todo");
+  const [sotti, setSotti] = useState(true);
+  const keteDao = () => {
+    setSotti(false);
+  };
+
+  if (sotti) {
+    return (
+      <>
+        <div>
+          <h1>
+            <span className="hoynai">{name}</span> - Not Done
+          </h1>
+          <button onClick={keteDao}>Mark as Done</button>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <h1>
+          <del className="hoise">{name}</del> - Done Bitch
+        </h1>
+        <button onClick={keteDao}>Mark as Done</button>
+      </div>
+    );
+  }
+}
+
+export default JodiEitaTaileOita;
