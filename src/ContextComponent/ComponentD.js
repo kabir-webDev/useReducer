83b205c9a66998cc9a431f3f01492d538cda9e5f
kabir-webDev <kabir.webdev@gmail.com>
2020-09-18 19:45:29 +0600

    Component D

--- src/ContextComponent/ComponentD.js
+++ src/ContextComponent/ComponentD.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function ComponentD() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h1>This is component D</h1>
+    </div>
+  );
 }
 
-export default ComponentD
+export default ComponentD;
