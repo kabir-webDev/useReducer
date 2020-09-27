f6614ddb2d817b3ee4b4be8b9de36f71224757a9
kabir-webDev <kabir.webdev@gmail.com>
2020-09-27 22:44:51 +0600

    Fetch

--- src/ContextComponent/ComponentD.js
+++ src/ContextComponent/ComponentD.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function ComponentD() {
+  const [user, setUser] = useState();
+  useEffect(() => {});
   return (
     <div>
       <h1>This is component D</h1>
