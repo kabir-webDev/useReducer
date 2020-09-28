5caee0fa1a2415cd0523fd0574aba179f92e7ea7
kabir-webDev <kabir.webdev@gmail.com>
2020-09-28 23:17:19 +0600

    Pokemon

---
+++ src/component/Pokemon/PokemonGo.js
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect } from "react";
+import { PokemonContext } from "./PokemonContext";
+
+const url = "https://pokeapi.co/api/v2/pokemon";
+
+export const PokemonsList = () => {
+  const { state, capture, addPokemons } = useContext(PokemonContext);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      addPokemons(data.results);
+    };
+
+    fetchPokemons();
+  }, [addPokemons]);
+
+  return (
+    <div className="pokemons-list">
+      <h2>Pokemons List</h2>
+
+      {state.pokemons.map((pokemon) => (
+        <div key={pokemon.name}>
+          <div>
+            <span>{pokemon.name}</span>
+            <button onClick={capture(pokemon)}>+</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PokemonsList;
