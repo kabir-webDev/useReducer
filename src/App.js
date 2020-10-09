88e7cca939cd2f84d6570ac5c547fb560c75d2e6
kabir-webDev <kabir.webdev@gmail.com>
2020-10-10 01:37:59 +0600

    Context API Testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import JodiEitaTaileOita from "./Conditional/JodiEitaTaileOita";
-// import { useState } from "react";
+// import JodiEitaTaileOita from "./Conditional/JodiEitaTaileOita";
+import { useState } from "react";
 // import TextHidden from "./component/Delete/TextHidden";
 // import Fetch from "./component/FetchData/Fetch";
 // import ComponentA from "./ContextComponent/ComponentA";
 // import ComponentB from "./ContextComponent/ComponentB";
-// import { UserContext } from "./ContextComponent/UserContext";
+import { UserContext } from "./ContextComponent/UserContext";
 // import ComponentE from "./ContextComponent/ComponentE";
 // import ReducerPrac from "./ReducerPrac";
 // import Another from "./component/Another.JS";
@@ -14,8 +14,7 @@ import JodiEitaTaileOita from "./Conditional/JodiEitaTaileOita";
 function App() {
   return (
     <div className="App">
-      {" "}
-      <JodiEitaTaileOita />{" "}
+      walah
     </div>
   );
 }
